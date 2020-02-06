7545e5b0f32510a6163c2d719a8636abaf764466
devr22 <devr52222@gmail.com>
2020-02-06 11:48:59 +0530

    updated

---
+++ app/src/main/java/com/example/notesappwithmvvm/NoteDAO.java
@@ -0,0 +1,30 @@
+package com.example.notesappwithmvvm;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDAO {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM Note_table")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM Note_table ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
+
+}
